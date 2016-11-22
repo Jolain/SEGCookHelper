5bcdc3b0ee92ff02c1b6eb5eab78046f85cdc245
Jolain Poirier <Jolain@users.noreply.github.com>
2016-11-22 12:53:47 -0500

    Added blank layout & activity files

---
+++ app/src/main/java/com/compilers/segcookhelper/AddRecipeActivity.java
@@ -0,0 +1,13 @@
+package com.compilers.segcookhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddRecipeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
---
+++ app/src/main/java/com/compilers/segcookhelper/RecipeViewActivity.java
@@ -0,0 +1,13 @@
+package com.compilers.segcookhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecipeViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+}
