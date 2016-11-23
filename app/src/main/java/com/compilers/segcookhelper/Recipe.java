5a61f3af042e3c58c8e49efdce522368c3ee83e0
WamboK <wkeng103@uottawa.ca>
2016-11-23 08:40:44 -0500

    Edited recipe_container layout, Recipe.class and created an recipeAdapter.class

--- app/src/main/java/com/compilers/segcookhelper/Recipe.java
+++ app/src/main/java/com/compilers/segcookhelper/Recipe.java
@@ -1,6 +1,6 @@
 package com.compilers.segcookhelper;
 
-import android.media.Image;
+
 
 import java.util.LinkedList;
 
@@ -12,11 +12,53 @@ public class Recipe {
 
     // **** Instance variable
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
     private String description;
-    private Image img; // Maybe not Image type
+    private int img; // Maybe not Image type
     private LinkedList<Ingredient> ingredients = new LinkedList<>();
     private LinkedList<Category> categories = new LinkedList<>();
+
+
+    public LinkedList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(LinkedList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LinkedList<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(LinkedList<Category> categories) {
+        this.categories = categories;
+    }
+
 
     // **** Constructor
 
---
+++ app/src/main/java/com/compilers/segcookhelper/RecipeAdapter.java
@@ -0,0 +1,41 @@
+package com.compilers.segcookhelper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Weierstrass on 2016-11-23.
+ */
+
+public class RecipeAdapter extends ArrayAdapter<Recipe>{
+
+    public RecipeAdapter(Context context, List<Recipe> recettes) {
+        super(context, R.layout.recipe_container,recettes );
+
+
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.activity_main, parent, false);
+        TextView recipeName = (TextView) rowView.findViewById(R.id.RecipeName);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.ImageRecipe);
+
+
+        Recipe recipe = this.getItem(position);
+        imageView.setImageResource(recipe.getImg());
+        recipeName.setText(recipe.getName());
+
+
+        return rowView;
+    }
+}
+
