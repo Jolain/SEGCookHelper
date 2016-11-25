14389b9e8ad7efb9e36f46253459e59ce11046ee
WamboK <wkeng103@uottawa.ca>
2016-11-24 23:39:18 -0500

    No major modification, just created some recipes to help me during the recipeview.layout

--- app/src/main/java/com/compilers/segcookhelper/RecipeViewActivity.java
+++ app/src/main/java/com/compilers/segcookhelper/RecipeViewActivity.java
@@ -1,12 +1,13 @@
 package com.compilers.segcookhelper;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class RecipeViewActivity extends AppCompatActivity {
+public class RecipeViewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
