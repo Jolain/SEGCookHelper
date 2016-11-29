e132a6837098e24556d0a2bcaf43331385531121
Jolain Poirier <Jolain@users.noreply.github.com>
2016-11-29 18:30:34 -0500

    Removed redundant methods

--- app/src/main/java/com/compilers/segcookhelper/MainActivity.java
+++ app/src/main/java/com/compilers/segcookhelper/MainActivity.java
@@ -25,11 +25,11 @@ public class MainActivity extends Activity {
         dbHelper.onLoad(dbHelper.getReadableDatabase());
         System.out.println("App started");
 
-        //TODO initialises categories on startup, should be in database
-        String[] items = {"chinese", "breakfast", "italian", "dinner", "collation", "cookies", "drink"};
-        for(int i=0;i<items.length;i++){
-            dbHelper.addCategory(new Category(items[i]));
-        }
+        // Database already initialized categories
+        // String[] items = {"chinese", "breakfast", "italian", "dinner", "collation", "cookies", "drink"};
+        // for(int i=0;i<items.length;i++){
+        //     dbHelper.addCategory(new Category(items[i]));
+        // }
     }
 
     public void onClickSearch(View view) {
